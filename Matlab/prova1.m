0e38762eebf1649927fda5c42bb34ab9a0784996
JoaoVitorSBarbosa <jvsb2017@gmail.com>
2025-05-06 16:14:37 -0300

    Corrige erros no codigo da prova

--- Matlab/prova1.m
+++ Matlab/prova1.m
@@ -6,10 +6,10 @@ B1 = [
         0  0  0  1;
         0  0  0  1;
     ];
-Rot4 = Rot('z',-90)
-Rot5 = Rot('x',-90)
+Rot4 = Rot('z',90)
+Rot5 = Rot('x',90)
 Trans2 = Trans(1,0,-1)
-B = B1 * Trans2 * Rot5 * Rot4
+B = Trans2 * Rot5 * Rot4
 
 Rot1 = Rot('z',45)
 Rot2 = Rot('y',30)
@@ -32,7 +32,9 @@ fi = 20;
 psi = 30;
 
 RAG1 = Rot('x',teta) * Rot('y',fi) * Rot('z',psi);
-RAG1 = double( RAG1 * Trans(1,2,3))
+RAG1 = double( Trans(1,2,3) * RAG1 )
+
+N = double(RAG1 * Trans(2,5,6))
 
 %% 2
 
@@ -46,6 +48,11 @@ H = [
 
 pc = PC(H,80);
 
+B = sym("B");
+r = sym("r");
+y = sym("y");
+
+A = Rot('z',y) * Rot('y',B) * Trans(0,0,r)
 F = [
         0  0  0  3;
         0  0  0  5;
@@ -53,7 +60,7 @@ F = [
         0  0  0  1;
     ];
 
-R03 = double(Rot('z',0) * Rot('y',48.25) * Trans(0,0,660.78));
+R03 = double(Rot('z',0) * Rot('y',48.25) * Trans(0,0,660.78))
 
 R06 = [0 0 1; 0 -1 0; 1 0 0];
 R03 = R03(1:3,1:3);
